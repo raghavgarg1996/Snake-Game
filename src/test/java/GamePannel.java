789963e8be784e4eaa418124b5ebd199b8368db6
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-12-06 09:46:41 +0530

    Setting Snake game

---
+++ src/test/java/GamePannel.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePannel extends JPanel implements ActionListener {
+
+    GamePannel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisons() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
